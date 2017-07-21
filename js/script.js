dd5f1d1cc49f8a2620b04c15d1cb87946eba8432
Chase Goddard <cwg45@cornell.edu>
2017-07-21 00:50:33 -0400

    fixed issue with text wrapping when window was too small

--- js/script.js
+++ js/script.js
@@ -4,22 +4,31 @@ $(document).ready(function(){
     $(".cs-courses").hide();
     $(".math-courses").hide();
     $("#physics-toggle").click(function(){
+        $("#physbot").show();
         if(phys_hidden){
             $(".physics-courses").show();
             $("#phys-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+            if($("#phystop").position().top - $("#physbot").position().top < -10) {
+                $("#physbot").hide();
+            }
         }
         else{
             $(".physics-courses").hide();
             $("#phys-chevron").removeClass("fa-chevron-up").addClass("fa-chevron-down");
         }
+        
         phys_hidden = !phys_hidden;
     });
 
     var cs_hidden = true;
     $("#cs-toggle").click(function(){
+        $("#csbot").show();
         if(cs_hidden){
             $(".cs-courses").show();
             $("#cs-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+            if($("#cstop").position().top - $("#csbot").position().top < -10) {
+                $("#csbot").hide();
+            }
         }
         else{
             $(".cs-courses").hide();
@@ -30,9 +39,13 @@ $(document).ready(function(){
 
     var math_hidden = true;
     $("#math-toggle").click(function(){
+        $("#mathbot").show();
         if(math_hidden){
             $(".math-courses").show();
             $("#math-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+            if($("#mathtop").position().top - $("#mathbot").position().top < -10) {
+                $("#mathbot").hide();
+            }
         }
         else{
             $(".math-courses").hide();
@@ -40,4 +53,6 @@ $(document).ready(function(){
         }
         math_hidden = !math_hidden;
     });
+
+
 });
