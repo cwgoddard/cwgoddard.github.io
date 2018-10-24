511918a44140e0f02001af5795c6cf93155ef160
Chase Goddard <cwg45@cornell.edu>
2018-10-24 13:58:53 -0400

    update coursework

--- js/script.js
+++ js/script.js
@@ -3,18 +3,28 @@ $(document).ready(function(){
     $(".physics-courses").hide();
     $(".cs-courses").hide();
     $(".math-courses").hide();
+    var phys_height = $("#phys").height();
     $("#physics-toggle").click(function(){
         $("#physbot").show();
         if(phys_hidden){
             $(".physics-courses").show();
             $("#phys-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-            if($("#phystop").position().top - $("#physbot").position().top < -10) {
-                $("#physbot").hide();
-            }
+            $("#phys").css({
+                height:phys_height + $("#phystop").height()
+            });
+            //if($("#phystop").position().top - $("#physbot").position().top < -30) {
+            //    $("#physbot").hide();
+            //}
+            //$("#phys").css({
+             //   height:$("#phystop").height() + $("#phys-chevron").height()+20
+            //});
         }
         else{
             $(".physics-courses").hide();
             $("#phys-chevron").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+            $("#phys").css({
+                height:phys_height
+            });
         }
         
         phys_hidden = !phys_hidden;
@@ -26,13 +36,19 @@ $(document).ready(function(){
         if(cs_hidden){
             $(".cs-courses").show();
             $("#cs-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-            if($("#cstop").position().top - $("#csbot").position().top < -10) {
-                $("#csbot").hide();
-            }
+            $("#cs").css({
+                height:$("#cs").innerHeight()
+            })
+//            if($("#cstop").position().top - $("#csbot").position().top < -10) {
+ //               $("#csbot").hide();
+  //          }
         }
         else{
             $(".cs-courses").hide();
             $("#cs-chevron").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+            $("#cs").css({
+                height:$("#cs").innerHeight()
+            })
         }
         cs_hidden = !cs_hidden;
     });
@@ -43,13 +59,19 @@ $(document).ready(function(){
         if(math_hidden){
             $(".math-courses").show();
             $("#math-chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-            if($("#mathtop").position().top - $("#mathbot").position().top < -10) {
-                $("#mathbot").hide();
-            }
+            $("#math").css({
+                height:$("#math").innerHeight()
+            })
+            //if($("#mathtop").position().top - $("#mathbot").position().top < -10) {
+             //   $("#mathbot").hide();
+            //}
         }
         else{
             $(".math-courses").hide();
             $("#math-chevron").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+            $("#math").css({
+                height:$("#math").innerHeight()
+            })
         }
         math_hidden = !math_hidden;
     });
